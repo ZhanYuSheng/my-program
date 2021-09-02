6dc9a704ccdb99e66268324f19480b739067e538
zhanyusheng <1143109382@qq.com>
2021-09-02 09:44:31 +0800

    vue commit

---
+++ vue-demo/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <img src="../assets/芝麻垮脸.jpg" height="200" width="200" >
+        <div>邮箱地址: 1143109382@qq.com</div>
+        <div>近期学习的 Demo 保存在github: <a href='https://github.com/ZhanYuSheng/my-program'>https://github.com/ZhanYuSheng/my-program</a></div>
+        <router-link to="/updateLog">更新日志</router-link>
+        <!-- 对应的组件内容渲染到router-view中 -->
+        <router-view/>
+    </div>
+</template>
+
+<style scoped>
+    .home{
+        text-align: center;
+        margin-top: 60px;
+    }
+</style>
---
+++ vue-demo/src/components/UpdateLog.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="title">更新日志</div>
+        <div>
+            <div>2021.08.25</div>
+            <ol>
+                <li>新建Home页</li>
+                <li>新建UpdateLog页</li>
+            </ol>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<style scoped>
+    .title{
+        text-align: center;
+        margin-top: 60px;
+    }
+</style>
